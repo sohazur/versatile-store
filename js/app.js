487e620aa46602927694554aa4dbc49115729809
Sohazur <mdsohazurislam@gmail.com>
2022-01-20 22:05:41 +0400

    Added the rating feature

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <p>Rating: ${product.rating.rate}/5 (${product.rating.count})</p>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
